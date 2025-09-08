95bbff42f4d968b91468504b52f2beb1555796ef
Uladzislau <u.shylko25@gmail.com>
2025-09-08 16:37:04 +0300

    feat: add method Map

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,17 @@ export type Task = {
 
 export const App = () => {
     const tasks1 = [
-        {id:1, title: 'HTML&CSS', isDone: true},
-        {id:2, title: 'JS', isDone: true},
-        {id:3, title: 'ReactJS', isDone: false},
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false},
+        {id: 4, title: 'Redux', isDone: false},
+        {id: 5, title: 'Typescript', isDone: false},
+        {id: 6, title: 'RTK query', isDone: false},
     ]
     const tasks2 = [
-        {id:1, title: 'Hello world', isDone: true},
-        {id:2, title: 'I am Happy', isDone: true},
-        {id:3, title: 'Yo', isDone: false},
+        {id: 1, title: 'Hello world', isDone: true},
+        {id: 2, title: 'I am Happy', isDone: true},
+        {id: 3, title: 'Yo', isDone: false},
     ]
 
     return (
