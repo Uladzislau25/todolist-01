3a99baa7ce14e5aa992d07cfbe6811c1166a9e4e
Uladzislau <u.shylko25@gmail.com>
2025-09-10 12:24:38 +0300

    feat: deleted array and tasks2

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,10 @@ export const App = () => {
         {id: 5, title: 'Typescript', isDone: false},
         {id: 6, title: 'RTK query', isDone: false},
     ]
-    const tasks2 = [
-        {id: 1, title: 'Hello world', isDone: true},
-        {id: 2, title: 'I am Happy', isDone: true},
-        {id: 3, title: 'Yo', isDone: false},
-    ]
 
     return (
         <div className="app">
             <TodolistItem title="What to learn" tasks={tasks1} date="27.01.2027"/>
-            <TodolistItem title="Songs" tasks={tasks2}/>
         </div>
     )
 }
